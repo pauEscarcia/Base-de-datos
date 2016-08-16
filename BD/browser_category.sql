2a4c09c8669ad88a8d824aab23d117eddeeffaf9
pauescarcia <paulina_151090@hotmail.com>
2016-08-15 23:33:56 -0500

    BD

---
+++ BD/browser_category.sql
@@ -0,0 +1,16 @@
+﻿-- Table: public.browser_category
+
+-- DROP TABLE public.browser_category;
+
+CREATE TABLE public.browser_category
+(
+  "idCategory" integer NOT NULL DEFAULT nextval('"browser_category_idCategory_seq"'::regclass),
+  name character varying(60) NOT NULL,
+  description text NOT NULL,
+  CONSTRAINT browser_category_pkey PRIMARY KEY ("idCategory")
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.browser_category
+  OWNER TO trixdiscover;
